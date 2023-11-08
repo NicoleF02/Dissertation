7d9d7b87e9e19e438f993b67d3d23e3fd0675dd8
NicoleF02 <NicoleFerguson2002@outlook.com>
2023-11-08 12:05:57 +0000

    Venn Diagrams included

--- learnR.R
+++ learnR.R
@@ -1,11 +1,27 @@
-"Hello World"
-# <- assigns variables
-clear <- "HelloWorld"
-print(clear)
-"temp" -> vara
+filter_table <- function(df, matching_values, key = "human.entrez") {
+  if (!key %in% colnames(df)) {
+    stop(paste("Column '", key, "' not found in the data frame."))
+  }
 
-# for (i in 1:10){
-#   print(i)
-# }
+  matching_rows <- df[df[, key] %in% matching_values, ]
+  return(matching_rows)
+}
 
-paste(vara,clear)
+# Example usage:
+data <- data.frame(
+  "human.entrez" = c(1, 2, 3),
+  "psd" = c("A", "B", "C"),
+  "pre" = c("X", "Y", "X"),
+  "syn" = c("Y", "Z", "Z")
+)
+
+matching_values <- c(1, 3)
+result <- filter_table(data, matching_values)
+
+print(result)
+
+
+
+
+
+
--- venn.R
+++ venn.R
@@ -7,6 +7,10 @@ library(data.table)
 library(RSQLite)
 library(DBI)
 library(VennDiagram)
+install.packages("ggplot2")
+install.packages("dplyr")
+library(ggplot2)
+library(dplyr)
 
 ilePathFullDB <- "Files"
 
@@ -108,6 +112,35 @@ matchingValues <- function(list1, list2){
   return(matching_list)
 }
 
+matchingValuesSynapse <- function(matchedList, synapseDB){
+  key <- "HUMAN.ENTREZ.ID"
+  matching_rows <- synapseDB[synapseDB[, key] %in% matchedList, ]
+  return(matching_rows)
+}
+
+vennSynapse <- function(vennSynapse){
+  psdpres <- sum(subset(vennSynapse, psd == 1 & pres == 1)$psd)
+  pressyn <- sum(subset(vennSynapse, syn == 1 & pres == 1)$syn)
+  psdsyn <- sum(subset(vennSynapse, psd == 1 & syn == 1)$psd)
+  psdpressyn <- sum(subset(vennSynapse, psd == 1 & pres == 1 & syn == 1)$psd)
+  psd <- sum(vennSynapse$psd)
+  pres <- sum(vennSynapse$pres)
+  syn <- sum(vennSynapse$syn)
+
+
+  grid.newpage()
+  draw.triple.venn(area1 = psd, area2 = pres, area3= syn,
+                   n12 = psdpres, n23= pressyn, n13 = psdsyn, n123=psdpressyn,
+                   fill = c('yellow','brown','blue'),
+                   category = c("PSD","PRES","SYN"))
+
+}
+
+filteredSynGo <- function(totalDB, dbSynList){
+  key <- "HumanEntrez"
+  matching_rows <- totalDB[totalDB[, key] %in% dbSynList, ]
+  return(matching_rows)
+}
 
 # Just join gene and disease
 # Extract list and filter it for non null, make sure its just for human.
@@ -119,7 +152,7 @@ dbTotal <- length(HumanEntrezDB)
 
 Trubetskoy_2022_broad_coding_total <- length(Trubetskoy_2022_broad_coding)
 
-Trubetskoy_2022_prioritised_coding <- length(Trubetskoy_2022_prioritised_coding)
+Trubetskoy_2022_prioritised_coding_total <- length(Trubetskoy_2022_prioritised_coding)
 
 Trubetskoy_broad_db_cross <- matchingValues(Trubetskoy_2022_broad_coding, HumanEntrezDB)
 Trubetskoy_broad_db_cross_total <- runningTotalMatching(Trubetskoy_2022_broad_coding, HumanEntrezDB)
@@ -128,11 +161,111 @@ Trubetskoy_broad_db_cross_total <- runningTotalMatching(Trubetskoy_2022_broad_co
 Trubetskoy_prioritised_db_cross <- matchingValues(Trubetskoy_2022_prioritised_coding, HumanEntrezDB)
 Trubetskoy_prioritised_db_cross_total <- runningTotalMatching(Trubetskoy_prioritised_db_cross, HumanEntrezDB)
 
+
+# This has the entire database as a flat file, we will use this to check for overlapping genes in general
+dbFile <- file.path("Files/Full_DB_Rat_Aut22.txt")
+synapseDB <- read.table(file=dbFile, sep="\t", header=TRUE)
+
+synapseList <- na.omit(extractColumnAsList(synapseDB, "HUMAN.ENTREZ.ID"))
+
+synapsePriortisedCompleteOverlap <- runningTotalMatching(Trubetskoy_broad_db_cross, synapseList)
+
+
+tableSynapsePriortisedOverlap <- matchingValuesSynapse(Trubetskoy_2022_prioritised_coding, synapseDB)
+tableSynapseBroadOverlap <- matchingValuesSynapse(Trubetskoy_2022_broad_coding, synapseDB)
+
+#vennSynapse(tableSynapsePriortisedOverlap)
+
+# Want to compare Full_DB_Rat and full database
+
+query <- "SELECT distinct HumanEntrez
+FROM FullGeneFullDisease"
+
+dbDataFull <- extractColumnAsList(dbGetQuery(db, query), "HumanEntrez")
+
+
+# We don't want distinct HumanEntrez as SynGO can be accosciated with more than one
+# query <- "SELECT HumanEntrez, SynGO, B.Des
+# FROM Gene A
+# LEFT JOIN GOGene B
+# ON A.
+# WHERE HumanEntrez is NOT NULL and SynGO is NOT NULL"
+#
+# dbDataSynGo <- dbGetQuery(db, query)
+# dbDataSynGoList <- extractColumnAsList(dbDataSynGo, "HumanEntrez")
+# synMatchBroad <- filteredSynGo(dbDataSynGo, Trubetskoy_2022_broad_coding)
+
+# We want which ones have synGO associations, so yeah, maybe also do most common syngo associations?
+
+#crossSynapseDBRAT <- matchingValuesSynapse(dbDataFull, synapseDB)
+
+query <- "SELECT a.GeneID, b.HumanEntrez, a.GOID
+FROM GOGene a
+LEFT JOIN Gene b on a.GeneID=b.ID
+WHERE GOID is NOT NULL"
+
+dbDataGo <- dbGetQuery(db, query)
+dbDataGoList <- extractColumnAsList(dbDataSynGo, "HumanEntrez")
+# broadGOCross <- matchingValues(dbDataGoList, Trubetskoy_2022_broad_coding)
+# # TODO possible bar on what GO associations
+# draw.pairwise.venn(area1 = Trubetskoy_2022_broad_coding_total, area2 = length(dbDataGoList),
+#                    cross.area = length(broadGOCross), fill = c('red','blue'),
+#                    category = c("Trubetskoy_2022_broad_coding","GO Genes"))
+
+
+prioristiedGOCross <- filteredSynGo(dbDataGoList, Trubetskoy_2022_prioritised_coding)
+
+
+
+data_count <- prioristiedGOCross %>%
+  group_by(GOID) %>%
+  summarize(count = n())
+
+ggplot(data_count, aes(x = GOID, y = count)) +
+  geom_bar(stat = "identity") +
+  labs(title = "Bar Chart of GOID Occurrences", x = "GOID", y = "Count")
+
+
+
+
+
+# draw.pairwise.venn(area1 = Trubetskoy_2022_prioritised_coding_total, area2 = length(dbDataGoList),
+#                    cross.area = length(prioristiedGOCross), fill = c('red','blue'),
+#                    category = c("Trubetskoy_2022_priortised_coding","GO Genes"))
+
+
+
+#vennSynapse(tableSynapsePriortisedOverlap)
+
+
+
+
+#
+# draw.pairwise.venn(area1 = length(dbDataFull), area2 = length(synapseList),
+#                    cross.area = length(crossSynapseDBRAT), fill = c('red','blue'),
+#                    category = c("Full_DB_Rat","Synaptic DB"))
+
+
+
+
+# draw.pairwise.venn(area1 = Trubetskoy_2022_prioritised_coding_total, area2 = length(synapseList),
+#                    cross.area = synapsePriortisedCompleteOverlap, fill = c('red','blue'),
+#                    category = c("Trubetskoy_2022_prioritised_coding_total","Synaptic DB Total"))
+
+
+
+
+# synapseBroadCompleteOverlap <- runningTotalMatching(Trubetskoy_2022_broad_coding, synapseList)
+# draw.pairwise.venn(area1 = Trubetskoy_2022_broad_coding_total, area2 = length(synapseList),
+#                    cross.area = synapseBroadCompleteOverlap, fill = c('red','blue'),
+#                    category = c("Trubetskoy_2022_broad_coding_total","Synaptic DB Total"))
+
 # draw.pairwise.venn(area1 = Trubetskoy_2022_broad_coding_total, area2 = dbTotal,
 #                    cross.area = Trubetskoy_broad_db_cross_total, fill = c('red','blue'),
 #                    category = c("Trubetskoy_2022_broad_coding_total","Synaptic DB"))
 
 
-draw.pairwise.venn(area1 = Trubetskoy_2022_prioritised_coding_total, area2 = dbTotal,
-                   cross.area = Trubetskoy_prioritised_db_cross_total, fill = c('red','blue'),
-                   category = c("Trubetskoy_2022_prioritised_coding_total","Synaptic DB"))
+# draw.pairwise.venn(area1 = Trubetskoy_2022_prioritised_coding_total, area2 = dbTotal,
+#                    cross.area = Trubetskoy_prioritised_db_cross_total, fill = c('red','blue'),
+#                    category = c("Trubetskoy_2022_prioritised_coding_total","Synaptic DB"))
+
