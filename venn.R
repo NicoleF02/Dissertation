98383d62d29bdee76b9f89870616105c9c151f90
NicoleF02 <NicoleFerguson2002@outlook.com>
2023-11-30 20:17:53 +0000

    Fixed some nitpicking

--- venn.R
+++ venn.R
@@ -10,75 +10,64 @@ library(VennDiagram)
 library(ggplot2)
 library(dplyr)
 
-ilePathFullDB <- "Files"
 
-dbFile <- file.path("Files/Full_DB_Rat_Aut22.txt")
-trubetskoyFilePath <- file.path("Files/Trubetskoy_2022_magma.txt")
+ReadTrubetskoyFile <- function(){
+  # Read the data from the text file
+  trubetskoyFilePath <- file.path("Files/Trubetskoy_2022_magma.txt")
 
-synapseDB <- read.table(file=dbFile, sep="\t", header=TRUE)
+  data <- readLines(trubetskoyFilePath)
 
-# Read the data from the text file
-data <- readLines(trubetskoyFilePath)
+  # Initialize a list to store the rows of Table 2
+  table2 <- list()
 
-# Initialize a list to store the rows of Table 2
-table2 <- list()
+  # Determine the maximum number of columns
+  max_cols <- 0
 
-# Determine the maximum number of columns
-max_cols <- 0
+  # Iterate through each line in Table 1
+  for (line in data) {
+    # Split the line based on the tab character "\t" to separate the row header
+    parts <- strsplit(line, "\t")[[1]]
 
-# Iterate through each line in Table 1
-for (line in data) {
-  # Split the line based on the tab character "\t" to separate the row header
-  parts <- strsplit(line, "\t")[[1]]
+    # The first part is the row header for Table 2
+    row_header <- parts[1]
 
-  # The first part is the row header for Table 2
-  row_header <- parts[1]
+    # The rest of the parts are space-separated values
+    values <- strsplit(parts[-1], " ")[[1]]
 
-  # The rest of the parts are space-separated values
-  values <- strsplit(parts[-1], " ")[[1]]
+    # Update the maximum number of columns if needed
+    max_cols <- max(max_cols, length(values))
 
-  # Update the maximum number of columns if needed
-  max_cols <- max(max_cols, length(values))
+    # Append the row to Table 2
+    table2[[row_header]] <- values
+  }
 
-  # Append the row to Table 2
-  table2[[row_header]] <- values
+  # Fill in missing values with NA to create a data frame
+  trubetskoyFile <- as.data.frame(
+    lapply(table2, function(x) {
+      if (length(x) < max_cols) {
+        c(x, rep(NA, max_cols - length(x)))
+      } else {
+        x
+      }
+    })
+  )
+
+  # Rename the columns with the row headers
+  colnames(trubetskoyFile) <- names(table2)
+
+  return(trubetskoyFile)
 }
 
-# Fill in missing values with NA to create a data frame
-trubetskoyFile <- as.data.frame(
-  lapply(table2, function(x) {
-    if (length(x) < max_cols) {
-      c(x, rep(NA, max_cols - length(x)))
-    } else {
-      x
-    }
-  })
-)
 
-# Rename the columns with the row headers
-colnames(trubetskoyFile) <- names(table2)
+ReadDatabase <- function (dbName="Database/DS_10283_3877/synaptic.proteome_SR_20210408.db.sqlite", query){
+  db <- dbConnect(SQLite(), dbname = "Database/DS_10283_3877/synaptic.proteome_SR_20210408.db.sqlite")
+  dbData <- dbGetQuery(db, query)
+
+  return(dbData)
+}
 
 
-db <- dbConnect(SQLite(), dbname = "Database/DS_10283_3877/synaptic.proteome_SR_20210408.db.sqlite")
-
-query <- "SELECT distinct HumanEntrez
-FROM FullGeneFullDisease
-WHERE HDOID = \"DOID:5419\""
-
-dbData <- dbGetQuery(db, query)
-
-# dbData
-# synapseDB
-# trubetskoyFile
-
-# we trubetskoyFile and dbData venn diagrams.
-grid.newpage()
-
-# Only do the coding ones, broad coding and prioritised coding.
-# Compare both of them seperately with the 3 current data sets, to get two venn diagrams from them.
-# As soon as they are compared, find which pre,post will have more schizophrenia genes for it. Another set with in syn go and not in syn go. Another venn diagram with the database
-
-extractColumnAsList <- function(data_frame, column_header) {
+ExtractColumnAsList <- function(data_frame, column_header) {
   if (column_header %in% names(data_frame)) {
     column_data <- data_frame[[column_header]]
     column_data <- as.integer(column_data)
@@ -89,7 +78,8 @@ extractColumnAsList <- function(data_frame, column_header) {
   }
 }
 
-runningTotalMatching <- function(list1, list2){
+
+RunningTotalMatching <- function(list1, list2){
   total <- 0
   for (value in list1){
     if (any(list2 == value, na.rm = TRUE)){
@@ -100,7 +90,8 @@ runningTotalMatching <- function(list1, list2){
   return(total)
 }
 
-matchingValues <- function(list1, list2){
+
+MatchingValues <- function(list1, list2){
   matching_list <- vector("numeric")
   for (value in list1){
     if (any(list2 == value, na.rm = TRUE)){
@@ -110,13 +101,15 @@ matchingValues <- function(list1, list2){
   return(matching_list)
 }
 
-matchingValuesSynapse <- function(matchedList, synapseDB){
+
+MatchingValuesSynapse <- function(matchedList, synapseDB){
   key <- "HUMAN.ENTREZ.ID"
   matching_rows <- synapseDB[synapseDB[, key] %in% matchedList, ]
   return(matching_rows)
 }
 
-vennSynapse <- function(vennSynapse){
+
+VennSynapse <- function(vennSynapse){
   psdpres <- sum(subset(vennSynapse, psd == 1 & pres == 1)$psd)
   pressyn <- sum(subset(vennSynapse, syn == 1 & pres == 1)$syn)
   psdsyn <- sum(subset(vennSynapse, psd == 1 & syn == 1)$psd)
@@ -134,14 +127,15 @@ vennSynapse <- function(vennSynapse){
 
 }
 
-filteredSynGo <- function(totalDB, dbSynList){
+
+FilteredSynGo <- function(totalDB, dbSynList){
   key <- "EntrezID_Human"
   matching_rows <- totalDB[totalDB[, key] %in% dbSynList, ]
   return(matching_rows)
 }
 
 
-barchart <- function(dataFrame, titleName, xLabel){
+Barchart <- function(dataFrame, titleName, xLabel){
   data_count <- dataFrame %>%
     group_by(SynGO) %>%
     summarize(count = n())
@@ -151,44 +145,59 @@ barchart <- function(dataFrame, titleName, xLabel){
     labs(title = titleName, x = xLabel, y = "Count")
 }
 
-# Just join gene and disease
-# Extract list and filter it for non null, make sure its just for human.
-Trubetskoy_2022_broad_coding <- na.omit(extractColumnAsList(trubetskoyFile, "Trubetskoy_2022_broad_coding"))
-Trubetskoy_2022_prioritised_coding <- na.omit(extractColumnAsList(trubetskoyFile, "Trubetskoy_2022_prioritised_coding"))
-HumanEntrezDB <- na.omit(extractColumnAsList(dbData, "HumanEntrez"))
 
+
+dbFile <- file.path("Files/Full_DB_Rat_Aut22.txt")
+HumanEntrezDB <- na.omit(ExtractColumnAsList(dbData, "HumanEntrez"))
 dbTotal <- length(HumanEntrezDB)
 
-Trubetskoy_2022_broad_coding_total <- length(Trubetskoy_2022_broad_coding)
 
+synapseDB <- read.table(file=dbFile, sep="\t", header=TRUE)
+
+grid.newpage()
+
+query <- "SELECT distinct HumanEntrez
+FROM FullGeneFullDisease
+WHERE HDOID = \"DOID:5419\""
+
+dbData <- ReadDatabase(query=query)
+
+# Just join gene and disease
+
+
+TrubetskoyFile <- ReadTrubetskoyFile()
+# Extract list and filter it for non null, make sure its just for human.
+Trubetskoy_2022_broad_coding <- na.omit(ExtractColumnAsList(trubetskoyFile, "Trubetskoy_2022_broad_coding"))
+Trubetskoy_2022_prioritised_coding <- na.omit(ExtractColumnAsList(trubetskoyFile, "Trubetskoy_2022_prioritised_coding"))
+
+Trubetskoy_2022_broad_coding_total <- length(Trubetskoy_2022_broad_coding)
 Trubetskoy_2022_prioritised_coding_total <- length(Trubetskoy_2022_prioritised_coding)
 
-Trubetskoy_broad_db_cross <- matchingValues(Trubetskoy_2022_broad_coding, HumanEntrezDB)
-Trubetskoy_broad_db_cross_total <- runningTotalMatching(Trubetskoy_2022_broad_coding, HumanEntrezDB)
+Trubetskoy_broad_db_cross <- MatchingValues(Trubetskoy_2022_broad_coding, HumanEntrezDB)
+Trubetskoy_broad_db_cross_total <- RunningTotalMatching(Trubetskoy_2022_broad_coding, HumanEntrezDB)
 
-
-Trubetskoy_prioritised_db_cross <- matchingValues(Trubetskoy_2022_prioritised_coding, HumanEntrezDB)
-Trubetskoy_prioritised_db_cross_total <- runningTotalMatching(Trubetskoy_prioritised_db_cross, HumanEntrezDB)
+Trubetskoy_prioritised_db_cross <- MatchingValues(Trubetskoy_2022_prioritised_coding, HumanEntrezDB)
+Trubetskoy_prioritised_db_cross_total <- RunningTotalMatching(Trubetskoy_prioritised_db_cross, HumanEntrezDB)
 
 
 # This has the entire database as a flat file, we will use this to check for overlapping genes in general
 dbFile <- file.path("Files/Full_DB_Rat_Aut22.txt")
 synapseDB <- read.table(file=dbFile, sep="\t", header=TRUE)
 
-synapseList <- na.omit(extractColumnAsList(synapseDB, "HUMAN.ENTREZ.ID"))
+synapseList <- na.omit(ExtractColumnAsList(synapseDB, "HUMAN.ENTREZ.ID"))
 
-synapsePriortisedCompleteOverlap <- runningTotalMatching(Trubetskoy_broad_db_cross, synapseList)
+synapsePriortisedCompleteOverlap <- RunningTotalMatching(Trubetskoy_broad_db_cross, synapseList)
 
 
-tableSynapsePriortisedOverlap <- matchingValuesSynapse(Trubetskoy_2022_prioritised_coding, synapseDB)
-tableSynapseBroadOverlap <- matchingValuesSynapse(Trubetskoy_2022_broad_coding, synapseDB)
+tableSynapsePriortisedOverlap <- MatchingValuesSynapse(Trubetskoy_2022_prioritised_coding, synapseDB)
+tableSynapseBroadOverlap <- MatchingValuesSynapse(Trubetskoy_2022_broad_coding, synapseDB)
 
 SynGOFile <- "Files/SynGO.txt"
 SynGO <- read.table(file=SynGOFile, sep="\t", header=TRUE)
 
 
 
-broadSynGOCross <- filteredSynGo(SynGO, Trubetskoy_2022_prioritised_coding)
+broadSynGOCross <- FilteredSynGo(SynGO, Trubetskoy_2022_prioritised_coding)
 #unique(data$Product)
 
 
